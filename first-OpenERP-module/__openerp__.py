a237dabd76cbfd0b8882f1e50ce68c3c59d78dbe
pankajJoshi <pankaj143342@gmail.com>
2014-03-07 16:16:16 +0530

    Create __openerp__.py

---
+++ first-OpenERP-module/__openerp__.py
@@ -0,0 +1,15 @@
+{
+    'name' : "Training_mngmnt_module",
+    'version' : "1.0",
+    'author' : "OpenERP _dex",
+    'category' : "Tools",
+    'depends' : ['base'],
+    'data' : [
+        'training_mngmnt_view.xml',
+#        'user_info_data.xml',
+#       'report/module_report.xml',
+#       'wizard/module_wizard.xml'
+    ],
+    'demo' : [],
+    'installable': True,
+}
